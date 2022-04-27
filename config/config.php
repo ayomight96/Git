aa1f8a0cd8d2775b80d23e21c6fa255049275e86
ADEKOYA <ayomide.adekoya@abbaandking.com>
2022-04-27 13:30:53 +0100

    Created a new file called test Did a composer update

--- config/config.php
+++ config/config.php
@@ -14,6 +14,13 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \Mezzio\Tooling\ConfigProvider::class,
+    \Mezzio\LaminasView\ConfigProvider::class,
+    \Mezzio\Helper\ConfigProvider::class,
+    \Mezzio\Router\LaminasRouter\ConfigProvider::class,
+    \Laminas\Router\ConfigProvider::class,
+    \Laminas\HttpHandlerRunner\ConfigProvider::class,
+    \Laminas\Validator\ConfigProvider::class,
     // Include cache configuration
     new ArrayProvider($cacheConfig),
     ConfigProvider::class,
